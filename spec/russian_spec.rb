6b557b95a0aa38491ee351775d4337aac19b0dab
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2011-04-20 15:14:47 +0400

    Fixed gemspec, rakefile and tests

--- spec/russian_spec.rb
+++ spec/russian_spec.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*- 
+# -*- encoding: utf-8 -*-
 
 require File.dirname(__FILE__) + '/spec_helper'
 
@@ -20,13 +20,13 @@ describe Russian do
       Russian.locale.should == Russian::LOCALE
     end
   end
-  
+
   describe "with custom backend class" do
     it "should define i18n_backend_class" do
       Russian.i18n_backend_class.should == I18n::Backend::Advanced
     end
   end
-  
+
   describe "during i18n initialization" do
     after(:each) do
       I18n.load_path = []
@@ -37,31 +37,31 @@ describe Russian do
       Russian.init_i18n
       I18n.backend.class.should == Russian.i18n_backend_class
     end
-    
+
     it "should keep existing translations while switching backends" do
       I18n.load_path << File.join(File.dirname(__FILE__), 'fixtures', 'en.yml')
       Russian.init_i18n
       I18n.t(:foo, :locale => :'en').should == "bar"
     end
-    
+
     it "should keep existing :ru translations while switching backends" do
       I18n.load_path << File.join(File.dirname(__FILE__), 'fixtures', 'ru.yml')
       Russian.init_i18n
       I18n.t(:'date.formats.default', :locale => :'ru').should == "override"
     end
-    
+
     it "should set default locale to Russian locale" do
       Russian.init_i18n
       I18n.default_locale.should == Russian.locale
     end
   end
-  
+
   describe "with localize proxy" do
     before(:all) do
-      @time = mock(:time)
+      @time = Time.new #mock(:time)
       @options = { :format => "%d %B %Y" }
     end
-    
+
     %w(l localize).each do |method|
       it "'#{method}' should call I18n backend localize" do
         I18n.should_receive(:localize).with(@time, @options.merge({ :locale => Russian.locale }))
@@ -69,7 +69,7 @@ describe Russian do
       end
     end
   end
-  
+
   describe "with translate proxy" do
     before(:all) do
       @object = :bar
@@ -83,10 +83,10 @@ describe Russian do
       end
     end
   end
-  
+
   describe "strftime" do
     before(:all) do
-      @time = mock(:time)
+      @time = Time.new # mock(:time)
     end
 
     it "should call localize with object and format" do
@@ -94,18 +94,18 @@ describe Russian do
       Russian.should_receive(:localize).with(@time, { :format => format })
       Russian.strftime(@time, format)
     end
-    
+
     it "should call localize with object and default format when format is not specified" do
       Russian.should_receive(:localize).with(@time, { :format => :default })
       Russian.strftime(@time)
     end
   end
-  
+
   describe "with pluralization" do
     %w(p pluralize).each do |method|
       it "'#{method}' should pluralize with variants given" do
         variants = %w(вещь вещи вещей вещи)
-        
+
         Russian.send(method, 1, *variants).should == "вещь"
         Russian.send(method, 2, *variants).should == 'вещи'
         Russian.send(method, 3, *variants).should == 'вещи'
@@ -117,12 +117,12 @@ describe Russian do
         Russian.send(method, 131, *variants).should == 'вещь'
         Russian.send(method, 3.14, *variants).should == 'вещи'
       end
-      
+
       it "should raise an exception when first parameter is not a number" do
         lambda { Russian.send(method, nil, "вещь", "вещи", "вещей") }.should raise_error(ArgumentError)
         lambda { Russian.send(method, "вещь", "вещь", "вещи", "вещей") }.should raise_error(ArgumentError)
       end
-      
+
       it "should raise an exception when there are not enough variants" do
         lambda { Russian.send(method, 1) }.should raise_error(ArgumentError)
         lambda { Russian.send(method, 1, "вещь") }.should raise_error(ArgumentError)

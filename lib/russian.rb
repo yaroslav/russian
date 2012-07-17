9e7603afab6fb0ed62ac9eb5fde2c29090488e36
Alexander Lomakin <alexander.lomakin@gmail.com>
2012-07-17 15:15:51 +0400

    Added ability to use %n as a marker to put input number in pluralization variants

--- lib/russian.rb
+++ lib/russian.rb
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*- 
+# -*- encoding: utf-8 -*-
 
 $KCODE = 'u' if RUBY_VERSION < "1.9"
 
@@ -9,9 +9,9 @@ require 'russian_rails'
 
 module Russian
   extend self
-  
+
   autoload :Transliteration, 'transliteration'
-  
+
   # Russian locale
   LOCALE = :'ru'
 
@@ -25,7 +25,7 @@ module Russian
   LOCALIZE_MONTH_NAMES_MATCH = /(%[-\d]?d|%e)(.*)(%B)/
   LOCALIZE_STANDALONE_ABBR_DAY_NAMES_MATCH = /^%a/
   LOCALIZE_STANDALONE_DAY_NAMES_MATCH = /^%A/
-    
+
   # Init Russian i18n: load all translations shipped with library.
   def init_i18n
     I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
@@ -39,30 +39,31 @@ module Russian
   # See I18n::translate
   def translate(key, options = {})
     I18n.translate(key, options.merge({ :locale => LOCALE }))
-  end        
+  end
   alias :t :translate
-  
+
   # See I18n::localize
   def localize(object, options = {})
     I18n.localize(object, options.merge({ :locale => LOCALE }))
   end
   alias :l :localize
-  
+
   # strftime() proxy with Russian localization
   def strftime(object, format = :default)
     localize(object, { :format => format })
   end
-  
+
   # Simple pluralization proxy
   #
-  # Usage: 
+  # Usage:
   #   Russian.pluralize(1, "вещь", "вещи", "вещей")
   #   Russian.pluralize(3.14, "вещь", "вещи", "вещей", "вещи")
+  #   Russina.pluralize(5, "Произошла %n ошибка", "Произошло %n ошибки", "Произошло %n ошибок")
   def pluralize(n, *variants)
     raise ArgumentError, "Must have a Numeric as a first parameter" unless n.is_a?(Numeric)
     raise ArgumentError, "Must have at least 3 variants for pluralization" if variants.size < 3
     raise ArgumentError, "Must have at least 4 variants for pluralization" if (variants.size < 4 && n != n.round)
-    variants_hash = pluralization_variants_to_hash(*variants)
+    variants_hash = pluralization_variants_to_hash(n, *variants)
     I18n.backend.send(:pluralize, LOCALE, variants_hash, n)
   end
   alias :p :pluralize
@@ -76,16 +77,17 @@ module Russian
     Russian::Transliteration.transliterate(str)
   end
   alias :translit :transliterate
-  
+
   protected
     # Returns all locale files shipped with library
     def locale_files
       Dir[File.join(File.dirname(__FILE__), "russian", "locale", "**/*")]
     end
-    
+
     # Converts an array of pluralization variants to a Hash that can be used
     # with I18n pluralization.
-    def pluralization_variants_to_hash(*variants)
+    def pluralization_variants_to_hash(n, *variants)
+      variants.map!{ |variant| variant.gsub '%n', n.to_s }
       {
         :one => variants[0],
         :few => variants[1],

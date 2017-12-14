2dec736b89bfe1a526d7ce199bb34cf3ab8acbb1
glebtv <glebtv@gmail.com>
2017-12-14 22:08:42 +0300

    fix date filters

--- lib/russian/rails_admin_datetime.rb
+++ lib/russian/rails_admin_datetime.rb
@@ -1,3 +1,30 @@
+require 'rails_admin/support/datetime'
+
+class RailsAdmin::Support::Datetime
+  class << self
+    alias_method :delocalize_without_russian, :delocalize
+    def delocalize(date_string, format)
+      ret = date_string
+      if I18n.locale == :ru
+        format.to_s.scan(/%[AaBbp]/) do |match|
+          case match
+          when '%B'
+            english = I18n.t('date.month_names', :locale => :en)[1..-1]
+            common_month_names = I18n.t('date.common_month_names')[1..-1]
+            common_month_names.each_with_index {|m, i| ret = ret.gsub(/#{m}/i, english[i]) } unless ret.blank?
+          when '%b'
+            english = I18n.t('date.abbr_month_names', :locale => :en)[1..-1]
+            common_abbr_month_names = I18n.t('date.common_abbr_month_names')[1..-1]
+            common_abbr_month_names.each_with_index {|m, i| ret = ret.gsub(/#{m}/i, english[i]) } unless ret.blank?
+          end
+        end
+      end
+      ret = delocalize_without_russian(ret, format)
+      ret
+    end
+  end
+end
+
 require 'rails_admin/config/fields/types/datetime'
 
 module RailsAdmin
@@ -5,25 +32,6 @@ module RailsAdmin
     module Fields
       module Types
         class Datetime < RailsAdmin::Config::Fields::Base
-          def parse_input(params)
-            str = params[name]
-            if I18n.locale == :ru
-              strftime_format.to_s.scan(/%[AaBbp]/) do |match|
-                case match
-                when '%B'
-                  english = I18n.t('date.month_names', :locale => :en)[1..-1]
-                  common_month_names = I18n.t('date.common_month_names')[1..-1]
-                  common_month_names.each_with_index {|m, i| str = str.gsub(/#{m}/i, english[i]) } unless str.blank?
-                when '%b'
-                  english = I18n.t('date.abbr_month_names', :locale => :en)[1..-1]
-                  common_abbr_month_names = I18n.t('date.common_abbr_month_names')[1..-1]
-                  common_abbr_month_names.each_with_index {|m, i| str = str.gsub(/#{m}/i, english[i]) } unless str.blank?
-                end
-              end
-            end
-            params[name] = parser.parse_string(str) if params[name]
-          end
-
           register_instance_option :formatted_value do
             ret = if time = (value || default_value)
               opt = {format: strftime_format, standalone: true}

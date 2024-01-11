6aee8bed8a7374b5bc6f931b476092b444e5fd22
Zhdanov <evdev34@gmail.com>
2024-01-11 11:10:44 +0300

    fix

--- lib/russian/russian_rails.rb
+++ lib/russian/russian_rails.rb
@@ -1,8 +1,4 @@
 # Rails hacks
-if defined?(ActiveModel)
-  require 'active_model_ext/custom_error_message'
-end
-
 if defined?(ActionView::Helpers)
-  require 'action_view_ext/helpers/date_helper' 
+  require 'action_view_ext/helpers/date_helper'
 end
